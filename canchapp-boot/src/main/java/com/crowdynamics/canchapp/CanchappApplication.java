7203f76e4bf0678aeeeebdd7cf39b18dcc8154ff
marcos <marcosrivas17@gmail.com>
2018-01-19 20:56:04 +0100

    new structure

--- canchapp-boot/src/main/java/com/crowdynamics/canchapp/CanchappApplication.java
+++ canchapp-boot/src/main/java/com/crowdynamics/canchapp/CanchappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CanchappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CanchappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CanchappApplication.class, args);
+    }
 }
